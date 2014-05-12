94163f78edbb3043c4bbacdd4cab787570cdff47
Roshan Bhandari <brishi98@gmail.com>
2014-05-12 10:51:23 +0545

    changes

--- apps/mainapp/urls.py
+++ apps/mainapp/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 
-    url(r'^test/$', 'apps.mainapp.views.dashboard'),
-    url(r'^attempt/$', 'apps.mainapp.views.attempt_question'),
+    url(r'test/$', 'apps.mainapp.views.dashboard'),
+    url(r'attempt/$', 'apps.mainapp.views.attempt_question'),
     url(r'^$', 'apps.mainapp.views.landing'),
+    url(r'exam/sample/$', 'apps.mainapp.views.exam_sample')
 )
