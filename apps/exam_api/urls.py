5c16aabb5605242ba9bb0f6cec764e88b5939952
rajsubit <subitraj8@gmail.com>
2014-05-15 13:57:14 +0545

    exam_api view modified for different handling requests

--- apps/exam_api/urls.py
+++ apps/exam_api/urls.py
@@ -6,24 +6,20 @@ urlpatterns = patterns(
     '',
 
     url(r'^$',
-        'apps.exam_api.views.list_exam_set',
+        'apps.exam_api.views.get_upcoming_exams',
         name='list_exams'),
     url(r'^(?P<exam_code>\d+)',
-        'apps.exam_api.views.get_questionset_from_database',
+        'apps.exam_api.views.get_question_set',
         name='get_model_question'),
     url(r'^load-questions',
-        'apps.exam_api.views.load_modelquestion_in_database',
+        'apps.exam_api.load_database.load_modelquestion_in_database',
         name='load_model_question'),
     url(r'^load-exam-data',
-        'apps.exam_api.views.load_examset_in_database',
+        'apps.exam_api.load_database.load_examset_in_database',
         name='load_exam_data'),
 
-    url(r'^$',
-        'apps.exam_api.views.list_exam_set',
-        name='list_exams'),
-
     url(r'^answers',
-        'apps.exam_api.views.check_answers',
+        'apps.exam_api.views.get_scores',
         name='check_answers'),
 
 )
--- apps/exam_api/views.py
+++ apps/exam_api/views.py
@@ -1,121 +1,80 @@
 import json
-import datetime
 
 from django.http import HttpResponse
 
 from apps.mainapp.classes.query_database import QuestionApi, ExammodelApi
 
 
-def load_examset_in_database(request):
+class ExamHandler():
     '''
-    the function is used to load exam set in the exam collection
-    of mcq database
+    The class performs activities related to a exam
     '''
-    exam_model = ExammodelApi()
-    exam_dict = [
-        {"exam_name": "IOE model exam 1",
-         "exam_date": 1399998500,
-         "image":"exam.jpg", 
-         "exam_code" : 100,
-         "exam_time" : "7.00pm",
-         "exam_category" : "",
-         },
-        {"exam_name": "IOE model exam 2",
-         "exam_date": 1399978500,
-         "image":"exam.jpg",         
-         "exam_code" : 101,
-         "exam_time" : "7.00pm",
-         "exam_category" : "",
-         },
-        {"exam_name": "IOE model exam 3",
-         "exam_date": 1399968500,
-         "image":"exam.jpg",         
-         "exam_code" : 102,
-         "exam_time" : "7.00pm",
-         "exam_category" : "",
-         },
-        {"exam_name": "IOE model exam 4",
-         "exam_date": 1399968500,
-         "image":"exam.jpg",         
-         "exam_code" : 103,
-         "exam_time" : "7.00pm",
-         "exam_category" : ""}
-    ]
-    exam_model.insert_new_model(exam_dict)
-    return HttpResponse("Exam model saved in the database")
-
-
-def load_modelquestion_in_database(request):
-    '''
-    the function is used to load fake data in question collection
-    of mcq database in mongodb
-    '''
-    f = open('apps/exam_api/question_testdata.json', 'rb')
-
-    json_obj = json.loads(f.read())
-    for i, x in enumerate(json_obj):
-        x['question_number'] = i + 1
-
-    question_api = QuestionApi()
-    question_api.insert_new_question(json_obj)
-    return HttpResponse("Question saved in the database")
-
-
-def get_questionset_from_database(request, exam_code, web_reponse=0):
-    '''
-    This function returns the questions with same examcode
-    '''
-    question_api = QuestionApi()
-    questions = question_api.find_all({"exam_code": int(exam_code)})
-    sorted_questions = sorted(questions, key=lambda k: k['question_number'])
-    if web_reponse == 0:
-        return HttpResponse(json.dumps(sorted_questions))
-    else:
+    def get_questionset_from_database(self, exam_code):
+        '''
+        This function returns the questions with same examcode
+        '''
+        question_api = QuestionApi()
+        questions = question_api.find_all_questions(
+            {"exam_code": int(exam_code)})
+        sorted_questions = sorted(
+            questions, key=lambda k: k['question_number'])
         return sorted_questions
 
-
-def list_exam_set(request, web_reponse=0):
-    '''
-    this function lists the available exam models
-    '''
-    exam_set = ExammodelApi()
-    exam_list = exam_set.find_all({})
-    if web_reponse == 0:
-        return HttpResponse(json.dumps(exam_list))
-    else:
+    def list_upcoming_exams(self):
+        '''
+        this function lists the available exam models
+        '''
+        exam_set = ExammodelApi()
+        exam_list = exam_set.find_all_exammodel({})
         return exam_list
 
+    def check_answers(self, exam_code, answer_list):
+        '''
+        This function receives list of answers and exam_code
+        and return the dictionary with correct answers of each subject
+        and sum of correct answers
+        '''
 
-def check_answers(request, web_reponse=0):
-    '''
-    This function receives list of answers and exam_code
-    and return the dictionary with correct answers of each subject
-    and sum of correct answers
-    '''
+        question_api = QuestionApi()
+        questions = question_api.find_all_questions(
+            {"exam_code": int(exam_code)})
+        sorted_questions = sorted(
+            questions, key=lambda k: k['question_number'])
+        correct_answers = {}
+        for index, choice in enumerate(answer_list):
+            if sorted_questions[index]['answer']['correct'] == choice:
+                try:
+                    correct_answers[sorted_questions[index]['subject']] += 1
+                except:
+                    correct_answers[sorted_questions[index]['subject']] = 1
+        total = 0
+        score_list = []
+        for key, value in correct_answers.iteritems():
+            temp = {}
+            temp['subject'] = key
+            temp['score'] = value
+            total += value
+            score_list.append(temp)
+        score_list.append({'subject': 'total', 'score': total})
+        return score_list
+
+
+def get_question_set(self, request, exam_code):
+    exam_handler = ExamHandler()
+    model_question_set = exam_handler.get_questionset_from_database(
+        exam_code)
+    return HttpResponse(json.dumps(model_question_set))
+
+
+def get_upcoming_exams(self, request):
+    exam_handler = ExamHandler()
+    upcoming_exams = exam_handler.list_upcoming_exams()
+    return HttpResponse(json.dumps(upcoming_exams))
+
+
+def get_scores(self, request):
     exam_code = request.GET.get('exam')
     answer_list = list(request.GET.get('answers'))
-    print 'answer_list', len(answer_list)
-    question_api = QuestionApi()
-    questions = question_api.find_all({"exam_code": int(exam_code)})
-    sorted_questions = sorted(questions, key=lambda k: k['question_number'])
-    correct_answers = {}
-    for index, choice in enumerate(answer_list):
-        if sorted_questions[index]['answer']['correct'] == choice:
-            try:
-                correct_answers[sorted_questions[index]['subject']] += 1
-            except:
-                correct_answers[sorted_questions[index]['subject']] = 1
-    total = 0
-    score_list = []
-    for key, value in correct_answers.iteritems():
-        temp = {}
-        temp['subject'] = key
-        temp['score'] = value
-        total += value
-        score_list.append(temp)
-    score_list.append({'subject': 'total', 'score': total})
-
-    if web_reponse == 0:
-        return HttpResponse(json.dumps(score_list))
-    else:
-        return score_list
+    exam_handler = ExamHandler()
+    score_dict = exam_handler.check_answers(exam_code, answer_list)
+    return HttpResponse(json.dumps(score_dict))
