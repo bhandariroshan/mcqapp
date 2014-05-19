74e9a0fcdbcdb27f62894a9c9e2790182a22761c
Roshan Bhandari <roshanbhandari@ubuntu.ubuntu-domain>
2014-05-19 14:45:31 +0545

    changes

--- apps/mainapp/urls.py
+++ apps/mainapp/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'apps.mainapp.views.landing'),
-    url(r'home/$', 'apps.mainapp.views.dashboard'),
+    url(r'^$', 'apps.mainapp.views.landing'),    
     url(r'attempt/$', 'apps.mainapp.views.attempt_question'),
     url(r'honorcode/(?P<exam_code>\w{1,40})/$', 'apps.mainapp.views.honorcode'),
     url(r'attend-exam/(?P<exam_code>\w{1,40})/$', 'apps.mainapp.views.attend_exam'),
