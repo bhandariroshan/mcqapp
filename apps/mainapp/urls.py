36610f57aa692bff8f75ff8bde96a4267ea84b86
Roshan Bhandari <brishi98@gmail.com>
2014-06-20 22:53:10 +0545

    changes

--- apps/mainapp/urls.py
+++ apps/mainapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'dps/(?P<exam_code>\w{1,40})/$', 'apps.mainapp.views.attend_dps_exam'),
     url(r'answers/(?P<exam_code>\w{1,40})/(?P<subject_name>\w{1,40})/$','apps.mainapp.views.show_result'),
     url(r'list-results/$','apps.mainapp.views.get_list_of_result'),
+    url(r'coupon/$','apps.mainapp.views.couponpage'),
     url(r'^ajax-handler/(?P<func_name>\w{1,40})$', 'apps.mainapp.home.ajax_request', name='ajax_handle'),	
     
 )
