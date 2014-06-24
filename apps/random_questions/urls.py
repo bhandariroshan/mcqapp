8a9a53dea56250a680866d06bfe77dda393ab846
rajsubit <subitraj8@gmail.com>
2014-06-24 14:14:51 +0545

    url for random question created

--- apps/random_questions/urls.py
+++ apps/random_questions/urls.py
@@ -5,21 +5,7 @@ urlpatterns = patterns(
 
     '',
 
-    url(r'^$',
-        'apps.exam_api.android_api.get_upcoming_exams',
-        name='list_exams'),
-    url(r'^(?P<exam_code>\d+)',
-        'apps.exam_api.android_api.get_question_set'),
-
-    url(r'^load-questions',
-        'apps.exam_api.load_database.load_modelquestion_in_database',
-        name='load_model_question'),
-    url(r'^load-exam-data',
-        'apps.exam_api.load_database.load_examset_in_database',
-        name='load_exam_data'),
-    url(r'^validate-coupon/(?P<exam_code>\w{6})$',
-        'apps.exam_api.android_api.validate_coupon'),
-    url(r'^answers',
-        'apps.exam_api.android_api.get_scores',
-        name='check_answers'),
+    url(r'^questions',
+        'apps.random_questions.views.generate_random_questions',
+        name='generate_random_questions'),
 )
