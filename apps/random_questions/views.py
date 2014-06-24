07132971bd8bb3b7ca839bc076ae62802ccb823a
rajsubit <subitraj8@gmail.com>
2014-06-24 14:07:07 +0545

    question sets of each exam model loaded

--- apps/random_questions/views.py
+++ apps/random_questions/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from apps.mainapp.classes.query_database import QuestionApi, ExammodelApi
+
+
+def generate_random_questions():
+    '''
+    The function generates generates a random question set by randomly picking
+    question from all exam sets
+    '''
+    exammodel_api = ExammodelApi()
+    exam_sets = exammodel_api.find_all_exammodel(
+        {'exam_family': 'DPS'})
+    question_sets = []
+    for each_set in exam_sets:
+        question_api = QuestionApi()
+        questions = question_api.find_all_questions(
+            {"exam_code": each_set['exam_code'], 'marks': 1},
+            fields={'answer.correct': 0}
+        )
+        question_sets.append(
+            sorted(questions, key=lambda k: k['question_number'])
+        )
+    return HttpResponse(question_sets)
