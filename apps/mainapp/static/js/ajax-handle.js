311e73d9e94f413aad73dc980571ee1e34ecab84
Roshan Bhandari <brishi98@gmail.com>
2014-06-25 03:28:10 +0545

    changes

--- apps/mainapp/static/js/ajax-handle.js
+++ apps/mainapp/static/js/ajax-handle.js
@@ -12,6 +12,62 @@ function ajax_request(s_handler, c_handler, input_data)
 function validate_coupon(exam_code, coupon_id){
 	ajax_request('validate_coupon', 'validate_coupon_success',{'exam_code':exam_code, 'coupon_code':coupon_id});
 }
+function get_next_page(exam_code,current, next){
+	ajax_request('get_nexp_page_of_questions', 'get_next_page_success', {'exam_code':exam_code, 'current':current, 'next':next});
+}
+
+
+function get_next_page_success(data){
+	data= jQuery.parseJSON(data);
+	$('#showExam').html('');
+	if (data['status'] == 'ok'){
+		$('#showExam').html(data['html']);
+		Gumby.initialize(['skiplink', 'checkbox', 'radiobtn'], true);
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);	
+		all_ans = data['all_ans'];
+		var q_no = []
+		for (var i = 0; i < all_ans.length; i++){
+    		q_no.push(all_ans[i]['q_no']);
+		}
+
+		for (var i = 0; i < all_ans.length; i++ ){
+			len = all_ans[i]['attempt_details'].length;
+			slected = all_ans[i]['attempt_details'][len-1]['selected_ans'];
+			check_id = "#radio_" + q_no[i] + '_' + slected.toUpperCase();
+  			$(check_id).trigger('gumby.check');  			
+		}
+		current_pg_num = data['current_pg_num'];
+
+	}
+
+}
+function load_result(exm_code){
+	ajax_request('load_result', 'load_result_success', {'exam_code':exm_code});
+}
+
+function get_unattempted_questions_number(exam_code){
+	ajax_request('get_unattempted_questions_number', 'get_unattempted_questions_number_success', {'exam_code':exam_code});
+}
+
+function get_unattempted_questions_number_success(data){
+	data = jQuery.parseJSON(data);
+	if(data['status'] == 'ok'){
+		$('#unAttemptedQuestions').html(data['questions']);
+		$('#remainingQuestionsClicked').html(data['questions']);
+		$('#remaining').html('<a href="javascript:void(0)" style="color:red" id="remained"> Remaining:</a>' + data['notattempted']);
+		$('#attempted').html('Attempted:: ' + data['attempted']);
+	}
+
+}
+function load_result_success(data){
+	data= jQuery.parseJSON(data);
+	$('#showExam').html('');
+	if (data['status'] == 'ok'){
+		$('#showExam').html(data['html']);
+		$('#showExam').show();
+		$('#ioe_system_ender').hide();
+	}
+}
 
 function validate_coupon_success(data){
 	data=jQuery.parseJSON(data);
@@ -43,6 +99,7 @@ function is_subscribed_success(data){
 function save_answer(exam_code, question_id, selected_ans,current_question_number){
 	ajax_request('save_answer', 'save_answer_success', 
 	{'qid':question_id, 'sans':selected_ans, 'exam_code':exam_code, 'current_question_number':current_question_number});
+
 }
 function save_answer_success(data){
 	data = jQuery.parseJSON(data);
@@ -51,7 +108,7 @@ function save_answer_success(data){
 	}
 	else{
 		toal_time = data['time_remained']*60;
-
+		get_unattempted_questions_number(exm_code);		
 	}
 }
 function ajax_honor_code_accept(exam_code){
