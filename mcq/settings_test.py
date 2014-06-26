02389bc61f2ff359a938afebcf13f611728f8372
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-06-26 18:09:44 +0545

    django-braces

--- mcq/settings_test.py
+++ mcq/settings_test.py
@@ -146,6 +146,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.twitter',
+    'django-braces',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
