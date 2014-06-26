a09a3ad71deef65bf24027e64385e9ad3f9c1cb4
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-06-26 18:50:54 +0545

    changes:

--- mcq/urls.py
+++ mcq/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-# from apps.mainapp.latest_users import LatestUser
 admin.autodiscover()
 
 urlpatterns = patterns(
