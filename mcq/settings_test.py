820521b4bfbf69235c9cedb5e76813bf17bd0c34
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-06-26 19:13:21 +0545

    changes

--- mcq/settings_test.py
+++ mcq/settings_test.py
@@ -127,7 +127,7 @@ ROOT_URLCONF = 'mcq.urls'
 WSGI_APPLICATION = 'mcq.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/srv/www/meroanswer/source/templates',
+    '/srv/www/mcq.phunka.com/application/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
