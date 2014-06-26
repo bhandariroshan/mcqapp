dabcee762a2041fdcc516fde9da4656884ba7418
rajsubit <subitraj8@gmail.com>
2014-06-26 17:04:09 +0545

    random question generated from all exam sets

--- apps/random_questions/views.py
+++ apps/random_questions/views.py
@@ -1,6 +1,6 @@
 import random
 
-from django.http import StreamingHttpResponse
+from django.http import HttpResponse
 
 from apps.mainapp.classes.query_database import QuestionApi, ExammodelApi
 
@@ -10,36 +10,29 @@ def generate_random_questions(request):
     The function generates generates a random question set by randomly picking
     question from all exam sets
     '''
-    # exammodel_api = ExammodelApi()
-    # exam_sets = exammodel_api.find_all_exammodel(
-    #     {'exam_family': 'DPS'})
-    # question_sets = []
-    # for each_set in exam_sets:
-    #     question_api = QuestionApi()
-    #     questions = question_api.find_all_questions(
-    #         {"exam_code": each_set['exam_code'], 'marks': 1},
-    #         fields={'question_number': 1, '_id': 0, 'exam_code': 1}
-    #     )
-    #     question_sets.append(sorted(
-    #         questions, key=lambda k: k['question_number']
-    #     ))
-    # arranged_question = []
-    # final_question_set = []
-    # if len(question_sets) == 0:
-    #     pass
-    # elif len(question_sets) == 1:
-    #     final_question_set = question_sets[0]
-    # else:
-    #     for i in range(len(question_sets[0])):
-    #         arranged_question.append(
-    #             [each_list[i] for each_list in question_sets]
-    #         )
-    #     for each_set in arranged_question:
-    #         final_question_set.append(
-    #             each_set[random.randrange(len(each_set))])
-    # return StreamingHttpResponse(final_question_set)
     exammodel_api = ExammodelApi()
-    exam_sets = exammodel_api.find_all_exammodel({}, fields={'exam_code': 1})
-    random_exam_codes = [exam_sets[random.randrange(len(exam_sets))]
-                         ['exam_code'] for i in range(65)]
-    print random_exam_codes.index(list(set(random_exam_codes))[0])
+    exam_sets = exammodel_api.find_all_exammodel(
+        {'exam_family': 'DPS'})
+    question_sets = []
+    for each_set in exam_sets:
+        question_api = QuestionApi()
+        questions = question_api.find_all_questions(
+            {"exam_code": each_set['exam_code'], 'marks': 1},
+            fields={'question_number': 1, '_id': 0, 'exam_code': 1}
+        )
+        if len(questions) != 0:
+            question_sets.append(
+                sorted(questions, key=lambda k: k['question_number'])
+            )
+    final_question_set = []
+    if len(question_sets) == 0:
+        pass
+    elif len(question_sets) == 1:
+        final_question_set = question_sets[0]
+    else:
+        for i in range(len(question_sets[0])):
+            final_question_set.append(
+                question_sets[random.randrange(len(question_sets))][i]
+            )
+
+    return HttpResponse(final_question_set)
