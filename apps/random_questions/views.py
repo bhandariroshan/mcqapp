e0cb69990c6ddcecdfc51f1b071bba3bd2c24c48
rajsubit <subitraj8@gmail.com>
2014-06-28 19:15:05 +0545

    random question generated and exam model saved in the database

--- apps/random_questions/views.py
+++ apps/random_questions/views.py
@@ -1,6 +1,8 @@
 import random
+import datetime
+import time
 
-from django.http import HttpResponse
+from bson.objectid import ObjectId
 
 from apps.mainapp.classes.query_database import QuestionApi, ExammodelApi
 
@@ -18,7 +20,7 @@ def generate_random_ioe_questions(request):
         question_api = QuestionApi()
         questions = question_api.find_all_questions(
             {"exam_code": each_set['exam_code'], 'marks': 1},
-            fields={'question_number': 1, '_id': 0, 'exam_code': 1}
+            fields={'question_number': 1}
         )
         if len(questions) != 0:
             question_sets.append(
@@ -32,7 +34,25 @@ def generate_random_ioe_questions(request):
     else:
         for i in range(len(question_sets[0])):
             final_question_set.append(
-                question_sets[random.randrange(len(question_sets))][i]
+                ObjectId(
+                    question_sets[
+                        random.randrange(len(question_sets))][i]['uid']['id']
+                )
             )
-
-    return HttpResponse(final_question_set)
+    new_exam_model = {
+        "exam_name": "IOE Practice set",
+        "exam_date": time.mktime(
+            datetime.datetime.now().timetuple()
+        ),
+        "image": "exam.jpg",
+        "exam_code": time.mktime(
+            datetime.datetime.now().timetuple()
+        ),
+        "exam_category": "BE-IOE",
+        "exam_duration": 60,
+        "exam_family": 'DPS',
+        "question_list": final_question_set
+    }
+    exammodel_api.insert_new_model(new_exam_model)
+    print new_exam_model['exam_code']
+    return final_question_set
