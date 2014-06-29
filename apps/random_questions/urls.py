a2ce4eab931a34326fec0d90be2c891db5672f67
rajsubit <subitraj8@gmail.com>
2014-06-29 09:52:10 +0545

    url to load question id list in exam model created

--- apps/random_questions/urls.py
+++ apps/random_questions/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns(
     url(r'^ioe-questions',
         'apps.random_questions.views.generate_random_ioe_questions',
         name='generate_random_ioe_questions'),
+    url(r'^load-question-id',
+        'apps.random_questions.views.add_questions_in_exam_model',
+        name='add_questions_in_exam_model'),
 )
