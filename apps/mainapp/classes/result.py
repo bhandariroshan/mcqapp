0ff85cac8d83cf8ae809c89e11a9bc06e9729d52
Roshan Bhandari <brishi98@gmail.com>
2014-07-09 17:27:05 +0545

    changes

--- apps/mainapp/classes/result.py
+++ apps/mainapp/classes/result.py
@@ -13,87 +13,87 @@ class Result():
     def find_ioe_exam_rank(self, exam_code, useruid):
         aggregation_pipeline = []
         result = self.db_object.aggregrate_all(self.table_name, [
-     			{'$match': {'exam_code' : int(exam_code)}},
-     			{ "$unwind": "$result" },
-			    { "$group": {
-		         "_id": "$_id",
-		         "result": { "$push": "$result" },
-		         "useruid": { "$first": "$useruid" },
-		         "exam_code": { "$first": "$exam_code" },
-		         "ess_time": { "$first": "$ess_time" },
-		         "Total": { 
-		             "$max": {
-		                 "$cond": [
-		                     { "$eq": [ "$result.subject", "Total" ] },
-		                     "$result.score",
-		                     0
-		                 ]
-		             }
-		         },
-		         "Physics": { 
-		             "$max": {
-		                 "$cond": [
-		                     { "$eq": [ "$result.subject", "Physics" ] },
-		                     "$result.score",
-		                     0
-		                 ]
-		             }
-		         },
-		         "Mathematics": { 
-		             "$max": {
-		                 "$cond": [
-		                     { "$eq": [ "$result.subject", "Mathematics" ] },
-		                     "$result.score",
-		                     0
-		                 ]
-		             }
-		         },
-		         "Chemistry": { 
-		             "$max": {
-		                 "$cond": [
-		                     { "$eq": [ "$result.subject", "Chemistry" ] },
-		                     "$result.score",
-		                     0
-		                 ]
-		             }
-		         },
-		         "English": { 
-		             "$max": {
-		                 "$cond": [
-		                     { "$eq": [ "$result.subject", "Biology" ] },
-		                     "$result.score",
-		                     0
-		                 ]
-		             }
-		         },
-		         "Aptitude": { 
-		             "$max": {
-		                 "$cond": [
-		                     { "$eq": [ "$result.subject", "Biology" ] },
-		                     "$result.score",
-		                     0
-		                 ]
-		             }
-		         }
-		     }},
+                {'$match': {'exam_code' : int(exam_code)}},
+                { "$unwind": "$result" },
+                { "$group": {
+                 "_id": "$_id",
+                 "result": { "$push": "$result" },
+                 "useruid": { "$first": "$useruid" },
+                 "exam_code": { "$first": "$exam_code" },
+                 "ess_time": { "$first": "$ess_time" },
+                 "Total": { 
+                     "$max": {
+                         "$cond": [
+                             { "$eq": [ "$result.subject", "Total" ] },
+                             "$result.score",
+                             0
+                         ]
+                     }
+                 },
+                 "Physics": { 
+                     "$max": {
+                         "$cond": [
+                             { "$eq": [ "$result.subject", "Physics" ] },
+                             "$result.score",
+                             0
+                         ]
+                     }
+                 },
+                 "Mathematics": { 
+                     "$max": {
+                         "$cond": [
+                             { "$eq": [ "$result.subject", "Mathematics" ] },
+                             "$result.score",
+                             0
+                         ]
+                     }
+                 },
+                 "Chemistry": { 
+                     "$max": {
+                         "$cond": [
+                             { "$eq": [ "$result.subject", "Chemistry" ] },
+                             "$result.score",
+                             0
+                         ]
+                     }
+                 },
+                 "English": { 
+                     "$max": {
+                         "$cond": [
+                             { "$eq": [ "$result.subject", "Biology" ] },
+                             "$result.score",
+                             0
+                         ]
+                     }
+                 },
+                 "Aptitude": { 
+                     "$max": {
+                         "$cond": [
+                             { "$eq": [ "$result.subject", "Biology" ] },
+                             "$result.score",
+                             0
+                         ]
+                     }
+                 }
+             }},
 
-		     
-		     { "$sort": {
-		         "Total": -1,
-		         "Physics": -1,
-		         "Mathematics": -1,
-		         "Chemistry": -1,
-		         "English": -1,
-		         "Aptitude": -1,
-		     }},
+             
+             { "$sort": {
+                 "Total": -1,
+                 "Physics": -1,
+                 "Mathematics": -1,
+                 "Chemistry": -1,
+                 "English": -1,
+                 "Aptitude": -1,
+             }},
 
-		     
-		     { "$project": {
-		         "result": 1,
-		         "useruid": 1,
-		         "exam_code": 1,
-		         "ess_time": 1
-		     }}
-		])
-		# rank = next(index for (index, d) in enumerate(result) if d["useruid"] == useruid)
-		return result
+             
+             { "$project": {
+                 "result": 1,
+                 "useruid": 1,
+                 "exam_code": 1,
+                 "ess_time": 1
+             }}
+        ])
+        # rank = next(index for (index, d) in enumerate(result) if d["useruid"] == useruid)
+        return result
