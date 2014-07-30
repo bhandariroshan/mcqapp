d6eef1c28d372c1f0b2392a2d8aa499eac634c60
rajsubit <subitraj8@gmail.com>
2014-07-30 14:45:37 +0545

    new class to calculate quiz result of the user

--- apps/quiz/user_quiz_data.py
+++ apps/quiz/user_quiz_data.py
@@ -16,9 +16,10 @@ class SaveQuiz():
         This function receives the user information, question id and option
         chosen by user for the question and saves the data in the database
         """
+
         question_id = request.POST.get('question_id')
         option = request.POST.get('option')
-        exam_code = request.POST.get('exam_code')
+        exam_code = int(request.POST.get('exam_code'))
         attempted_date = time.mktime(datetime.datetime.now().date().timetuple())
         quiz_answer_obj, created = QuizAnswer.objects.get_or_create(
             question_id=ObjectId(question_id),
@@ -29,3 +30,21 @@ class SaveQuiz():
         if not created:
             quiz_answer_obj.attempted_option = option
             quiz_answer_obj.save()
+
+
+class QuizResult():
+    """
+    This class is used for result related queries of of quiz
+    """
+
+    def user_quiz_result(self, request):
+        """
+        This function receives user information and exam code
+        and returns the result of the quiz for that quiz.
+        """
+
+        exam_code = int(request.POST.get('exam_code'))
+        user_quiz_answers = QuizAnswer.objects(
+            exam_code=exam_code,
+            user_id=request.user.id
+        )
