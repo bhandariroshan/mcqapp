52701e708a30945c9c6b32fd3c8a0412c34997e8
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-07-31 11:44:46 +0545

    mail errors disabled

---
+++ apps/mainapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+
+# EXAM_CHOICES = ('Medical', 'Engineering')
+EXAM_CHOICES = (('BE-IOE', 'BE-IOE',),
+                ('MBBS-IOM', 'MBBS-IOM'))
+
+class SignupForm(forms.Form):
+    
+    username = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': u'Username',
+     'class' : 'form-control'}))
+
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': u'Email',
+     'class' : 'form-control'}))
+
+
+    # exam_type = forms.MultipleChoiceField(required=True,
+    #     widget=forms.CheckboxSelectMultiple, choices=EXAM_CHOICES)
+
+
+    def signup(self, request, user):
+        print 'signup function called'
