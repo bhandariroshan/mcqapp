26b224704fd4632d55b134e88ede565243846761
rajsubit <subitraj8@gmail.com>
2014-07-31 12:10:07 +0545

    new method created to calculate user history of quiz

--- apps/quiz/user_leaderboard.py
+++ apps/quiz/user_leaderboard.py
@@ -1,7 +1,24 @@
-
-
 class LeaderBoard():
     """
     This class is used for leader board related operations
     """
-    pass
+
+    def user_quiz_result(self, request):
+        """
+        The function receives the user id and provides the quiz information
+        of the user
+        """
+        from django.contrib.auth.models import User
+        from .models import QuizResult
+
+        parameters = {}
+        try:
+            user = User.objects.get(id=request.user.id)
+        except:
+            return False
+        parameters['user'] = user
+        parameters['quiz_result'] = QuizResult.objects.filter(
+            user_id=user.id).order_by('exam_date')
+        parameters['total_score'] = QuizResult.objects.filter(
+            user_id=user.id).sum('exam_score')
+        return parameters
