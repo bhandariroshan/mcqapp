ecb634446bc5ccd35df5f9e8d6e6d0c2331ef581
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-08-05 16:55:51 +0545

    quiz winners

--- apps/quiz/quiz_result_admin.py
+++ apps/quiz/quiz_result_admin.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
-from apps.mainapp.classes.Userprofile import UserProfile
+# from apps.mainapp.classes.Userprofile import UserProfile
 from .models import QuizResult
+from datetime import datetime, timedelta
 
 
 class QuizResultAdminView(View):
@@ -12,10 +13,34 @@ class QuizResultAdminView(View):
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def get(self, request, *args, **kwargs):
         parameters = {}
-        user_profile_obj = UserProfile()
-        user = user_profile_obj.get_user_by_username(request.user.username)
+        # user_profile_obj = UserProfile()
+        # user = user_profile_obj.get_user_by_username(request.user.username)
+        # ioe_results = QuizResult.objects.filter(quiz_type="BE-IOE").order_by('attempted_date')
+        # iom_results = QuizResult.objects.filter(quiz_type="MBBS-IOM").order_by('attempted_date')
         all_results = QuizResult.objects.all().order_by('attempted_date')
-        for each in all_results:
-            print each.attempted_date
-        parameters['all_results'] = all_results
+        # for each in ioe_results:
+        #     each['attempted_date'] = datetime.datetime.fromtimestamp(
+        #         int(each['attempted_date'])
+        #     )
+        final_result = []
+        for each_result in all_results:
+            index = -1
+            each_result['attempted_date'] = datetime.fromtimestamp(
+                int(each_result['attempted_date'])
+            ).date()
+            for count, each in enumerate(final_result):
+                if each['attempted_date'] == each_result['attempted_date']:
+                    index = count
+            if index != -1:
+                final_result[index]['item'].append(each_result)
+            else:
+                final_result.append({
+                    'date': each_result['attempted_date'],
+                    'item': [each_result]
+                })
+        # parameters['ioe_results'] = ioe_results
+        # parameters['ioe_results'] = ioe_results
+        parameters['final_result'] = final_result
+        parameters['today'] = datetime.today().date()
+        parameters['yesterday'] = parameters['today'] - timedelta(1)
         return render(request, self.template_name, parameters)
