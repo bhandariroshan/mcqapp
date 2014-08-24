2bef38be0a2e23c2258d42485c5ed5ef47efb08b
Roshan Bhandari <brishi98@gmail.com>
2014-08-24 16:07:59 +0545

    changes

--- apps/mainapp/slc.py
+++ apps/mainapp/slc.py
@@ -10,14 +10,14 @@ from django.shortcuts import render_to_response
 
 @csrf_exempt
 def find_result(request): 
-    if request.method == "GET":
+    if request.method == "POST":
         cookie_jar = cookielib.CookieJar()
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
         urllib2.install_opener(opener)
 
-        number = request.GET.get('number')
-        dob = request.GET.get('dob')
-        eyear = request.GET.get('eyear')
+        number = request.POST.get('number')
+        dob = request.POST.get('dob')
+        eyear = request.POST.get('eyear')
         submit = request.POST.get('submit')
         device_id = request.POST.get('deviceId')
         email = request.POST.get('email')        
@@ -52,7 +52,6 @@ def find_result(request):
         else:            
             from slcntc import get_html_ntc
             result = get_html_ntc(number, dob.replace('/','-'))
-            # return HttpResponse(json.dumps({'result':result}))
                         
         if result.get('status') == "ok":
             result_request_success = ResultRequestSuccess()
